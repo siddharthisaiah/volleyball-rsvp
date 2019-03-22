9f146fb99bad781a196af3255b4145a849a6d46f
Siddharth <siddharthisaiah@gmail.com>
2019-03-22 23:57:48 +0530

    mock login page

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http:localhost:8888/volleyball-rsvp/';
+$config['base_url'] = 'http://localhost:8888/volleyball-rsvp';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,6 +35,7 @@ $config['base_url'] = 'http:localhost:8888/volleyball-rsvp/';
 | variable so that it is blank.
 |
 */
+// $config['index_page'] = 'index.php';
 $config['index_page'] = '';
 
 /*
@@ -380,7 +381,7 @@ $config['encryption_key'] = '';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = sys_get_temp_dir();
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
@@ -489,6 +490,8 @@ $config['compress_output'] = FALSE;
 |
 */
 $config['time_reference'] = 'local';
+date_default_timezone_set('Asia/Kolkata');
+
 
 /*
 |--------------------------------------------------------------------------
